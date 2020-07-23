beae4610bd9715c09581d4f079afbb37f64a1ef7
JRommmm <juanromanutti@Juans-MacBook-Pro.local>
2020-07-23 14:55:47 -0400

    3.15 fix

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ app.post('/api/persons', (request, response) => {
 
 
 app.delete('/api/persons/:id', (request, response, next) => {
+  
+	console.log("1")
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
