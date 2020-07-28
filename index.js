970561f6c9ceefa7703d644a1544a500ddfc33d5
JRommmm <juanromanutti@Juans-MacBook-Pro.local>
2020-07-28 16:45:14 -0400

    id update

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post('/api/persons', (request, response) => {
   const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
+    //id: generateId(),
   })
   //console.log("3")
   person.save().then(savedPerson => {
