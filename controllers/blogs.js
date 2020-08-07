8ba0deb2ce687e2849e17f8cb2f1400a92121478
JRommmm <juanromanutti@Juans-MacBook-Pro.local>
2020-08-07 18:09:56 -0400

    exercise 4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -10,6 +10,8 @@ blogsRouter.get('/', (request, response) => {
 })
 
 blogsRouter.post('/', (request, response) => {
+  
+  //console.log('inside post')
   const blog = new Blog(request.body)
 
   blog
@@ -17,6 +19,47 @@ blogsRouter.post('/', (request, response) => {
     .then(result => {
       response.status(201).json(result)
     })
+})
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  //console.log("inside put")
+  const body = request.body
+  //console.log(body.likes)
+  const newLikes = {
+    likes: body.likes
+  }
+
+/* //this works!
+  Blog.findByIdAndUpdate(request.params.id, newLikes, { new: true })
+  .then(updatedBlog => {
+    response.json(updatedBlog)
+  })
+  .catch(error => next(error))
+*/
+
+  
+  try{
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newLikes, { new: true })
+
+    if (updatedBlog){
+   response.json(updatedBlog) 
+  } else {
+      response.status(404).end()
+    }
+}
+  catch(exception){
+    next(exception)
+  }
+
 })
 
 
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
@@ -14,7 +15,7 @@ test('blogs are returned as json', async () => {
 test('there are 0 blogs', async () => {
   const response = await api.get('/api/blogs')
   console.log("test running in blog_api.test.js")
-  expect(response.body).toHaveLength(0)
+  expect(response.body).toHaveLength(4)
 })
 
 /*
@@ -22,6 +23,31 @@ test('id is defined', () => {
   expect(fetchNewFlavorIdea()).toBeDefined();
 });
 */
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "first",
+	author: "bob",
+	url: "www.fart.com",
+	likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+
+  const blogsAtEnd = await helper.blogsInDb()
+/*  
+  expect(blogsAtEnd).toHaveLength(helper.initialNotes.length + 1)
+*/
+  const contents = blogsAtEnd.map(n => n.title)
+  expect(contents).toContain(
+    "first"
+  )
+})
 
 
 afterAll(() => {
